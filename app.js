93b1e8fc5cf0b56240ad27b05e120ff04ae2cd17
Dale Simkin <dale.simkin32@gmail.com>
2025-09-08 16:43:42 +0100

    event workshop more mouse events

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ button.addEventListener("click", function (event) {
 button.addEventListener("mouseover", function (event) {
   console.log(event);
 });
+
+button.addEventListener("wheel", function (event) {
+  console.log(event);
+});
+
+button.addEventListener("dragover", function (event) {
+  console.log(event);
+});
