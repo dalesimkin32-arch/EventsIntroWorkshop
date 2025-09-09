2ae87cca59ce4b5175a4f6ed1f1e1e1ca7f137c2
Dale Simkin <dale.simkin32@gmail.com>
2025-09-09 10:25:03 +0100

    added on mouse leave to restore button text mover

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ console.log(button.textContent);
 
 button.addEventListener("click", function (event) {
   console.log(event);
+  button.textContent = "Get Off Me!";
 });
 
 button.addEventListener("mouseover", function (event) {
   console.log(event);
+  button.textContent = "Stop Tickling";
+});
+
+button.addEventListener("mouseleave", function (event) {
+  console.log(event);
+  button.textContent = "Click me!";
 });
 
 button.addEventListener("wheel", function (event) {
   console.log(event);
-});
-
-button.addEventListener("dragover", function (event) {
-  console.log(event);
+  button.textContent = "Big Wheels keep on Rollin'";
 });
